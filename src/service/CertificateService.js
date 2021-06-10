9fdd3bcfc4947e6cb7f58639baa66ce153661934
Brunno Oliveira <brunnogoncalves@gmail.com>
2021-06-09 23:03:43 -0300

    fix: disable puppeteer sandbox

--- src/service/CertificateService.js
+++ src/service/CertificateService.js
@@ -22,7 +22,9 @@ class CertificateService {
   }
 
   async getPdfFromHtml(html) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
     const page = await browser.newPage();
     await page.setContent(html);
     const pdf = await page.pdf({
