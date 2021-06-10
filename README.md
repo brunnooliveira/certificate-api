ffdedb920dd51cd609678ca39b132d236ecb517f
Brunno Oliveira <brunnogoncalves@gmail.com>
2021-06-09 22:45:13 -0300

    feat: add endpoint to emit cetificate

---
+++ README.md
@@ -0,0 +1,25 @@
+## Certificate
+
+APi para emissão de certificados
+
+**Rodando o projeto:**
+
+```
+npm install && npm start
+```
+
+**Endpoint:**
+
+```
+POST http://localhost:3333/certificado
+{
+    "html":"https://brunnooliveira.github.io/certificate.html",
+    "data": {
+        "name": "Brunno Oliveira",
+        "date": "09/06/2021",
+        "course": "NodeJS & Express"
+    }
+}
+```
+
+**Heroku**
---
+++ src/service/CertificateService.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+const puppeteer = require('puppeteer');
+const handlebars = require('handlebars');
+
+class CertificateService {
+  async emit(html, data) {
+    console.log(html);
+    const htmlContent = await this.getHtmlContent(html);
+    const filledHtml = await this.fillHtmlData(htmlContent, data);
+    const pdf = this.getPdfFromHtml(filledHtml);
+    return pdf;
+  }
+
+  async getHtmlContent(url) {
+    const response = await axios.get(url);
+    return response.data;
+  }
+
+  async fillHtmlData(html, data) {
+    const template = handlebars.compile(html);
+    return template(data);
+  }
+
+  async getPdfFromHtml(html) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(html);
+    const pdf = await page.pdf({
+      printBackground: true,
+      format: 'A4',
+      landscape: true,
+    });
+    await browser.close();
+    return pdf;
+  }
+}
+module.exports = new CertificateService();
